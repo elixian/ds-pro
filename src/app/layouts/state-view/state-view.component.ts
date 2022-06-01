523198cb914353e27dfc7dc9824151b8a9ebcb2e
Franck COLOMBO <franck.colombo@cnav.fr>
2022-06-01 14:52:34 +0200

    state

---
+++ src/app/layouts/state-view/state-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'state-view',
+  templateUrl: './state-view.component.html',
+  styleUrls: ['./state-view.component.scss']
+})
+export class StateViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
