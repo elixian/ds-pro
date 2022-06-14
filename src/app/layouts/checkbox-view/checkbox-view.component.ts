77eeb612d3d1d8d166fea27959aa493360ab1530
Franck COLOMBO <franck.colombo@cnav.fr>
2022-06-14 16:51:57 +0200

    new view

---
+++ src/app/layouts/checkbox-view/checkbox-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-view',
+  templateUrl: './checkbox-view.component.html',
+  styleUrls: ['./checkbox-view.component.scss']
+})
+export class CheckboxViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
