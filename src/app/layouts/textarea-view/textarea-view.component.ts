ed9037e63c8f70289fa6329c2f2fc7f1a85fa1c3
Franck COLOMBO <franck.colombo@cnav.fr>
2022-06-21 18:20:09 +0200

    viewer textarea

---
+++ src/app/layouts/textarea-view/textarea-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea-view',
+  templateUrl: './textarea-view.component.html',
+  styleUrls: ['./textarea-view.component.scss']
+})
+export class TextareaViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
